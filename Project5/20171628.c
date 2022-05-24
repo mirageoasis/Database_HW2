da721f2de599ffd95b15938961a0d74a8c4c9caf
mirageoasis <kimhw0820@naver.com>
2022-05-25 02:18:19 +0900

    테이블에 넣을 자료 만드는 중

--- Project5/20171628.c
+++ Project5/20171628.c
@@ -8,7 +8,9 @@ const char* host = "localhost";
 const char* user = "root";
 const char* pw = "password";
 const char* db = "testing"; //여기다가 원하는 스키마 이름 넣기 
-const char* file_name = "curd.txt";
+
+const char* create_table = "curd.txt";
+const char* insert_data = "insert.txt";
 const char* use_schema = "use testing";
 
 MYSQL* connection = NULL;
@@ -31,18 +33,16 @@ int main(void) {
 		printf("%d ERROR : %s\n", mysql_errno(&conn), mysql_error(&conn));
 		return 1;
 	}
-	else
-	{
-		printf("Connection Succeed\n");
-		// 쿼리 실행
-		file_reader(file_name);// read create table
-	}
-	//
+	
+	
+	printf("Connection Succeed\n");
+	// 쿼리 실행
+	file_reader(create_table);// read create table
+	
+	//for(int i = 0; i < ;i++)
+	file_reader(insert_data);
 
-
-
-
-
+	
 	mysql_query(connection, "drop SCHEMA testing"); //테이블 폭파
 	mysql_close(connection);
 	return 0;
